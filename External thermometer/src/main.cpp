d241c15177b52235ca876fe3deb0bf2095dc5bf0
alexmikh <alexmikh89@gmail.com>
2024-01-24 23:25:49 +0200

    The thermosensor changed to KY-013

--- External thermometer/src/main.cpp
+++ External thermometer/src/main.cpp
@@ -1,67 +1,33 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+// GPIO0->GND(for upload)/3v3
+// GPIO15->GND
+// EN->3v3
+// ADC -> KY-0.13
+
 #include "Arduino.h"
-#include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
+#include <KY013.h>
 
-#ifdef ESP32
-#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
-#error Select ESP8266 board.
-#endif
-
-DHTesp dht;
+KY013 thermistor(PIN_A0);
 
 void setup()
 {
-  // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-    Serial.begin(115200);
-  Serial.println();
-  Serial.printf("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
-  String thisBoard= ARDUINO_BOARD;
-  Serial.println(thisBoard);
-
-  // Autodetect is not working reliable, don't use the following line
-  // dht.setup(17);
-  // use this instead: 
-  dht.setup( D1, DHTesp::DHT11); // Connect DHT sensor to GPIO 17
+  Serial.begin(115200);
 }
 
 void loop()
 {
-    delay(dht.getMinimumSamplingPeriod());
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
+  int rawADC = analogRead(PIN_A0);
+  Serial.print("rawADC: ");
+  Serial.println(rawADC);
 
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(humidity, 1);
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.print(dht.toFahrenheit(temperature), 1);
-  Serial.print("\t\t");
-  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
-  Serial.print("\t\t");
-  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+  float temp = thermistor.readTemperatureC();
 
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print("temp: ");
+  Serial.print(temp);
+  Serial.println(" C");
 
-  // wait for a second
-  delay(500);
-
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  delay(1000);
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(500);
+  delay(2000);
 }
