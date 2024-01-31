45ceb995456ab4dc37af46b529da888d9ea4e047
alexmikh <alexmikh89@gmail.com>
2024-01-31 21:58:37 +0200

    Adjust KY-013 values indoors

--- External thermometer/src/main.cpp
+++ External thermometer/src/main.cpp
@@ -2,7 +2,7 @@
 // GPIO0->GND(for upload)/3v3(for execution)
 // GPIO15->GND
 // EN->3v3
-// ADC -> KY-0.13 via devder: 'KY-0.13 pin' + 5k + 'ESP ADC pin' + 10k + GND
+// ADC -> KY-0.13 via devider: 'KY-0.13 pin' + 4k7 + 'ESP ADC pin' + 2k + GND
 
 #include "Arduino.h"
 #include <KY013.h>
@@ -18,11 +18,8 @@ void loop()
 {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
-  float temp = thermistor.readTemperatureC();
-
-  Serial.print("temp: ");
+  float temp = thermistor.readTemperatureC() - 19;
   Serial.print(temp);
   Serial.println(" C");
-
-  delay(2000);
+  delay(1000);
 }
