dc75853502dced9e611d07a0aaae2cc69833da1f
alexmikh <alexmikh89@gmail.com>
2024-01-31 22:26:42 +0200

    Add shifter 74HC_HCT4067

--- External thermometer/src/main.cpp
+++ External thermometer/src/main.cpp
@@ -6,20 +6,31 @@
 
 #include "Arduino.h"
 #include <KY013.h>
+#include "MUX74HC4067.h"
 
 KY013 thermistor(PIN_A0);
+MUX74HC4067 mux(12, 14, 0, 4, 5); // IO12 IO14 IO00 IO04 IO05 -> EN S0 S1 S2 S3
 
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(115200);
+  mux.signalPin(13, OUTPUT, DIGITAL); //  IO13 -> SIG
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
   float temp = thermistor.readTemperatureC() - 19;
   Serial.print(temp);
   Serial.println(" C");
-  delay(1000);
+  delay(500);
+
+  for (byte i = 0; i < 16; ++i)
+  {
+    mux.write(i, HIGH); // Connects to channel i and writes HIGH
+    delay(50);
+    mux.write(i, LOW); // Connects to channel i and writes LOW
+    delay(50);
+  }
 }
